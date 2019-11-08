2110b4fa90c63970b945796218fe3484aaed269a
Rolando <rolando.umana@gmail.com>
2019-11-07 22:47:23 -0700

    aws credentials are now environment variables. reader.s3 now reads from s3 bucket and return urls to main

--- reader/s3.go
+++ reader/s3.go
@@ -1,9 +1,8 @@
 package reader
 
 import (
-	"fmt"
 	"os"
-
+	"bufio"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -12,13 +11,16 @@ import (
 
 type s3Bucket struct{}
 
+const tmpFile string = "/tmp/downtime_domains"
+
 func (r *s3Bucket) GetDomains() ([]string, error) {
+
 	sess, err := createAWSSession()
 	if err != nil {
 		return nil, err
 	}
 
-	file, err := os.Create("/tmp/downtime_domains")
+	file, err := os.Create(tmpFile)
 	if err != nil {
 		return nil, err
 	}
@@ -28,14 +30,22 @@ func (r *s3Bucket) GetDomains() ([]string, error) {
 	numBytes, err := downloader.Download(file,
 		&s3.GetObjectInput{
 			Bucket: aws.String(os.Getenv("DOWNTIME_S3_BUCKET")),
-			Key:    aws.String("DOWNTIME_S3_KEY")})
+			Key:    aws.String(os.Getenv("DOWNTIME_S3_KEY"))})
 	if err != nil {
-		return nil, err
+		return  nil, err
 	}
 
-	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+	if numBytes == 0 {
+		return []string{}, nil
+	}
 
-	return []string{}, nil
+	urls := []string{}
+	data := bufio.NewScanner(file)
+	for data.Scan() {
+		urls = append(urls, data.Text())
+	}
+
+	return urls, nil
 }
 
 func createAWSSession() (*session.Session, error) {
